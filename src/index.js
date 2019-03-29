08f17327e10d3d894544cb4fb234978413f1462b
Eric Mendes Dantas <ericdantas0@gmail.com>
2019-03-29 17:17:25 -0300

    exposes $floatingAlert in vue's component instance

--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
-import VueFloatingAlert from './floating-alert.vue'
+import VueFloatingAlert, {floatingAlert} from './floating-alert.vue'
 
 export default {
     install(Vue) {
+        Vue.prototype.$floatingAlert = {
+            error(opts) {
+                floatingAlert.error(opts)
+            },
+            success(opts) {
+                floatingAlert.success(opts)
+            },
+            info(opts) {
+                floatingAlert.info(opts)
+            },
+            warn(opts) {
+                floatingAlert.warn(opts)
+            },
+            hide() {
+                floatingAlert.hide(opts)
+            }
+        }
+
         Vue.component(VueFloatingAlert.name, VueFloatingAlert)
     }
 }
